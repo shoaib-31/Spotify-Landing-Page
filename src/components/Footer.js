fe71a4b547e39fbe7bb40e529cdc319dc34af6b5
Shoaib <shoaibakmasood@gmail.com>
2023-04-30 17:18:28 +0530

    Responsiveness left

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import "../styles/footer.css";
 import footer_logo from "../images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEarthAsia } from "@fortawesome/free-solid-svg-icons";
-import { faInstagram,faFacebook,faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <>
@@ -12,26 +16,43 @@ export default function Footer() {
           <div className="footer_logo">
             <img src={footer_logo} className="flogo" alt="logo" />
           </div>
-          <div className="footer_grid"></div>
-          <div className="footer_handles">
-            <div className="social">
-              <FontAwesomeIcon
-                icon={faInstagram}/>
+          <div className="footer_main">
+            <div className="footer_sub">
+              <div className="sub_head">COMPANY</div>
+              <div className="sub_head_items">About</div>
+              <div className="sub_head_items">Jobs</div>
+              <div className="sub_head_items">For the Records</div>
             </div>
-            <div className="social">
-              <FontAwesomeIcon
-                icon={faTwitter}
-              />
+            <div className="footer_sub">
+              <div className="sub_head">COMMUNITIES</div>
+              <div className="sub_head_items">For Artists</div>
+              <div className="sub_head_items">Developers</div>
+              <div className="sub_head_items">Advertising</div>
+              <div className="sub_head_items">Investors</div>
+              <div className="sub_head_items">Vendors</div>
+              <div className="sub_head_items">Spotify for Work</div>
             </div>
-            <div className="social">
-              <FontAwesomeIcon
-                icon={faFacebook}
-              />
+            <div className="footer_sub">
+              <div className="sub_head">USEFUL LINKS</div>
+              <div className="sub_head_items">Support</div>
+              <div className="sub_head_items">Web Player</div>
+              <div className="sub_head_items">Free Mobile App</div>
             </div>
           </div>
-          <div className="footer_language hg">
+          <div className="footer_handles">
+            <div className="social">
+              <FontAwesomeIcon icon={faInstagram} />
+            </div>
+            <div className="social">
+              <FontAwesomeIcon icon={faTwitter} />
+            </div>
+            <div className="social">
+              <FontAwesomeIcon icon={faFacebook} />
+            </div>
+          </div>
+          <div className="footer_language">
             <FontAwesomeIcon icon={faEarthAsia} style={{ color: "#cccccc" }} />
-            &nbsp; India (English)
+            &nbsp; <div className="hg">India (English)</div>
           </div>
           <div className="footer_info">
             <div className="f-info_left">

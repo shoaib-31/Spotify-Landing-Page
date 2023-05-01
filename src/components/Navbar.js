821d09c193cfbe5cf23021fd5d066c0977a3a430
Shoaib <shoaibakmasood@gmail.com>
2023-05-02 03:51:37 +0530

    Sidebar remains

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,43 +5,65 @@ import {
   faCircleUser,
   faAngleDown,
   faAngleUp,
+  faBars
 } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
+import MediaQuery from 'react-responsive';
 
 export default function Navbar() {
   const [Show, setShow] = useState(false);
   return (
     <>
-      <div className="main">
-        <div className="left">
-          <img src={logo} alt="logo" className="logo" />
-        </div>
-        <div className="right">
-          <div className="items">Premium</div>
-          <div className="items">Support</div>
-          <div className="items">Download</div>
-          <div>|</div>
-          <div
-            onClick={() => setShow(!Show)}
-            className="items"
-            style={{ color: Show ? "rgb(48, 150, 48)" : "aliceblue" }}
-          >
-            <FontAwesomeIcon className="profile" icon={faCircleUser} />
-            &nbsp; Profile &nbsp;{" "}
-            {Show ? (
-              <FontAwesomeIcon icon={faAngleUp} />
-            ) : (
-              <FontAwesomeIcon icon={faAngleDown} />
-            )}
-            {Show &&(
-            <div className="dropdown">
-              <div className="hover_green" style={{ color: "black" }}>Account</div>
-              <div className="hover_green" style={{ color: "rgb(160,160,160)" }}>Log out</div>
+      <MediaQuery minWidth={991}>
+        <div className="main">
+          <div className="left">
+            <img src={logo} alt="logo" className="logo" />
+          </div>
+          <div className="right">
+            <div className="items">Premium</div>
+            <div className="items">Support</div>
+            <div className="items">Download</div>
+            <div>|</div>
+            <div
+              onClick={() => setShow(!Show)}
+              className="items"
+              style={{ color: Show ? "rgb(48, 150, 48)" : "aliceblue" }}
+            >
+              <FontAwesomeIcon className="profile" icon={faCircleUser} />
+              &nbsp; Profile &nbsp;{" "}
+              {Show ? (
+                <FontAwesomeIcon icon={faAngleUp} />
+              ) : (
+                <FontAwesomeIcon icon={faAngleDown} />
+              )}
+              {Show && (
+                <div className="dropdown">
+                  <div className="hover_green" style={{ color: "black" }}>
+                    Account
+                  </div>
+                  <div
+                    className="hover_green"
+                    style={{ color: "rgb(160,160,160)" }}
+                  >
+                    Log out
+                  </div>
+                </div>
+              )}
             </div>
-)}
           </div>
         </div>
-      </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={991}>
+        <div className="main">
+          <div className="left">
+          <img src={logo} alt="logo" className="logo" />
+          </div>
+          <div className="right">
+          <FontAwesomeIcon className="profile" icon={faCircleUser} />
+          <FontAwesomeIcon icon={faBars} />
+          </div>
+        </div>
+      </MediaQuery>
     </>
   );
 }
